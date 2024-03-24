8388287f91dd78953fd0c703e51c1d05f99c3e60
lucashien <lucas641212@gmail.com>
2024-03-24 21:02:11 +0800

    add massenger

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import myImage from './img/img.jpg'; // 確保使用正確的路徑
+
 
 function App() {
+  // State for messages and counter
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
+  const [counter, setCounter] = useState(0);
+  console.log("ini",counter)
+  // Increment the counter each time the component mounts
+  useEffect(() => {
+    setCounter(prev => prev + 1);
+  }, []);
+
+  // Handle new message input changes
+  const handleNewMessageChange = (event) => {
+    setNewMessage(event.target.value);
+  };
+
+  // Add a new message to the message board
+  const handlePostMessage = () => {
+    if (newMessage) {
+      setMessages(prevMessages => [...prevMessages, newMessage]);
+      setNewMessage('');
+    }
+  };
+  console.log("bef return ",counter)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+          <img src={myImage} alt="Your Name" className="App-logo" />
+          <p className="App-intro">
+            Hi, I'm Tsung Han Hsieh. Welcome to my personal page!
+          </p>
+          <div className="Visiter-counter">
+            Visitor Count: {counter}
+          </div>
+          
+          <div className="App-message-board">
+          <h2>Live a Message!</h2>
+          <ul className="App-messages">
+            {messages.map((message, index) => (
+              <li key={index} className="App-message">
+                <div className="App-message-text">{message}</div>
+              </li>
+            ))}
+          </ul>
+
+          <div>
+            <input
+              type="text"
+              value={newMessage}
+              onChange={handleNewMessageChange}
+              placeholder="Type your message here..."
+            />
+            <button onClick={handlePostMessage}>Send</button>
+          </div>
+        </div>
       </header>
     </div>
   );
